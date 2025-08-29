efb216e1f0b89f3642b15a0affd4ba206e475e80
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-08-29 20:50:47 +0530

    Create check_cmds.sh

---
+++ scripts/check_cmds.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+check() {
+    for cmd in "$@"; do
+        printf "checking for %s... " "$cmd"
+        if command -v "$cmd" >/dev/null 2>&1; then
+            echo "found ($cmd)"
+        else
+            echo "not found"
+            echo "Error: '$cmd' is missing. Aborting."
+            exit 1
+        fi
+    done
+}
+
