2e57ebaef4b98f5d9c4340fdda33286691d0ad3a
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-09-02 18:48:21 +0530

    add LICENSE

--- scripts/gen_module.sh
+++ scripts/gen_module.sh
@@ -161,7 +161,8 @@ else
 fi
 
 FINAL_ZIP="${FFMPEG_VERSION}-${type}-android-${ANDROID_ABI}.zip"
-zip -r "${FINAL_ZIP}" META-INF ffmpeg.tar.xz customize.sh module.prop
+cp "${BUILD_DIR}/FFmpeg/COPYING.GPLv2 ./LICENSE
+zip -r "${FINAL_ZIP}" META-INF ffmpeg.tar.xz customize.sh module.prop LICENSE
 shopt -s extglob
 rm -rf !("$FINAL_ZIP")
 shopt -u extglob
